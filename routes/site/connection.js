6b06cfa4cb6bd2040cbd15504738365c8b6abbd9
vincent-leguevel <vincent.leguevel.sio@gmail.com>
2017-05-30 16:06:09 +0200

    gestion connexion / inscription terminée

--- routes/site/connection.js
+++ routes/site/connection.js
@@ -5,39 +5,44 @@ var express = require('express');
 var router = express.Router();
 var moduleUser = require('../../private/modules/Users');
 
+// Titre de la page
+const PAGE_TITLE = "Connexion";
+
 /**
  * Affiche la page de connexion
  */
 router.get('/',function (req, res, next) {
-    res.render('connection');
+    res.render('connection',{title : PAGE_TITLE});
 });
 
 /**
  * Connexion d'un utilisateur
  */
 router.post('/',function (req, res, next) {
-
+    let params = {title : PAGE_TITLE};
     moduleUser.connection(req.body.email, req.body.pwd)
         .then((user)=>{
             return res.status(user.errorCode).json(user);
         })
         .catch((err)=>{
-            let params = {};
+
+            // Renvoie les données sur les champs complétés ou met le nom du champ en rouge
+            if(req.body.email) params.email = req.body.email;
+            if(!req.body.email) params.missingEmail = true;
+            if(!req.body.pwd) params.missingPwd = true;
+
             if(err.errorInfo === "BAD PASSWORD OR MAIL") {
                 params.error = "Email ou mot de passe incorrect. Veuillez réessayer.";
-                params.badPwdOrMail  = true;
-                params.email =  req.body.email;
-                return res.render('connection',params);
-            }
-            else if(err.errorInfo === "INVALID INFORMATIONS"){
-                let params = {error : "Tous les champs doiveny être renseignés."};
-                if(req.body.email) params.email = req.body.email;
-                if(!req.body.email) params.missingEmail = true;
-                if(req.body.pwd) params.pwd = req.body.pwd;
-                if(!req.body.pwd) params.missingPwd = true;
+                params.missingEmail  = true;
+                params.missingPwd = true;
 
-                return res.render('connection',params);
+            } else if(err.errorInfo === "INVALID INFORMATIONS") {
+                params.error = "Tous les champs doivent être renseignés.";
+
+            } else {
+                params.error = "Une erreur est survenue. Veuillez réessayer.";
             }
+            return res.render('connection',params);
         });
 });
 
--- routes/site/inscription.js
+++ routes/site/inscription.js
@@ -5,32 +5,60 @@ let express = require('express');
 let router = express.Router();
 let moduleUser = require('../../private/modules/Users');
 
+const PAGE_TITLE = "Inscription";
+
 /**
  * Affiche la page d'inscription
  */
 router.get('/',function (req, res, next) {
-    res.render('inscription');
+    res.render('inscription',{title : PAGE_TITLE});
 });
 
 /**
  * Inscription d'un utilisateur
  */
 router.post('/',function (req, res, next) {
-
-    if(!req.body.email || !req.body.confEmail || !req.body.pwd || !req.body.confPwd || !req.body.pseudo){
-        let params = {missingFields : true};
-        return res.render('inscription',params);
-    }
-    console.log(req.body);
+    let params = {title : PAGE_TITLE};
     moduleUser.addUser(req.body.pseudo,req.body.firstName,req.body.lastName,
                         req.body.email,req.body.confEmail,req.body.pwd,req.body.confPwd)
-
         .then((user)=>{
             console.log('then test');
             console.log(user);
         })
         .catch((err)=>{
-            console.log(err);
+
+            // Renvoie les données sur les champs complétés ou met le nom du champ en rouge
+            if(req.body.pseudo) params.pseudo = req.body.pseudo;
+            if(!req.body.pseudo) params.missingPseudo = true;
+            if(req.body.email) params.email = req.body.email;
+            if(!req.body.email) params.missingEmail = true;
+            if(req.body.confEmail) params.confEmail = req.body.confEmail;
+            if(!req.body.confEmail) params.missingConfEmail = true;
+            if(req.body.pwd) params.pwd = req.body.pwd;
+            if(!req.body.pwd) params.missingPwd = true;
+            if(req.body.confPwd) params.confPwd = req.body.confPwd;
+            if(!req.body.confPwd) params.missingConfPwd = true;
+
+            if(err.errorInfo === "INVALID INFORMATIONS") {
+                params.error = "Les champs obligatoires doivent être renseignés";
+
+            } else if (err.errorInfo === "Email does not match") {
+                params.error = "Les emails ne correspondent pas";
+                params.missingEmail = true;
+                params.missingConfEmail = true;
+
+            } else if (err.errorInfo === "Password does not match") {
+                params.error = "Les mots de passe ne correspondent pas";
+                params.missingPwd = true;
+                params.missingConfPwd = true;
+            } else if (err.errorInfo === "MAIL OR PSEUDO ALREADY USED") {
+                params.error = "pseudo ou mail déjà utilisé";
+                params.missingPseudo = true;
+                params.missingEmail = true;
+            } else {
+                params.error = "Une erreur est survenue. Veuillez réessayer.";
+            }
+            return res.render('inscription',params);
         });
 });
 
